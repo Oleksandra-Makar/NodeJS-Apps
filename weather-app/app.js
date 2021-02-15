b3b83d33e11c77e4dc1304b2cd970ef6606ed8ff
Олександра Макар <oleksandra_makar@epam.com>
2021-02-16 01:03:29 +0200

    Added some functionality

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,27 +1,25 @@
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 const request = require('request')
 
-// const url = 'http://api.weatherstack.com/current?access_key=0a15b290210892a20bb9a08406b9910c&query=37.8267,-122.4233&units=f'
+const address = process.argv[2]
 
-// request({ url: url, json: true }, (error, response) =>{
-//     if (error) {
-//         console.log('Unable to connect to weather service!')
-//     } else if (response.body.error) {
-//         console.log('Unable to find location')
-//     } else {
-//         console.log(`${response.body.current.weather_descriptions[0]}. It is currently ${response.body.current.temperature} degrees out. It feels like ${response.body.current.feelslike} degrees out.`)
-//     }
-// })
+if (!address) {
+    console.log('Please provide an address')
+} else {
+    geocode(address, (error, { latitude, longitude, location } = {}) => {
+        if (error) {
+            return console.log(error)
+        } 
+        forecast(latitude,longitude, (error, forecastData) => {
+            if (error) {
+                return console.log(error)
+            }
+            console.log(location)
+            console.log(forecastData)
+        })
+    })
+}
 
-const geo = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1Ijoib2xla3NhbmRyYW1ha2FyIiwiYSI6ImNrbDVpYnRlODBvbGUybnA2aWE5NnV1ZDcifQ.XpLEGQrU9XwwCVWKqMzxPA&limit=1'
-request({ url: geo, json: true }, (error, response) =>{
-    if (error) {
-        console.log('Unable to connect to weather service!')
-    } else if (response.body.features.length === 0) {
-        console.log('Unable to find location. Try another search.')
-    } else {
-        const latitude = response.body.features[0].center[1]
-        const longitude = response.body.features[0].center[0]
 
-        console.log(`Latitude is: ${latitude}. Longitude is: ${longitude}`)
-    }
-})
+
